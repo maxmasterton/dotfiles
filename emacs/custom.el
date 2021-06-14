970152205f7b5d8512efbb61a44034002a47a9c1
Max Masterton <maxm@freebsd-desktop.home>
2021-06-14 16:57:55 +0100

    added complete emacs/exwm configuration and zsh configuration, removed everything else

---
+++ emacs/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(pyvenv eterm-256color writeroom-mode winum which-key vterm use-package typescript-mode treemacs-evil treemacs-all-the-icons telephone-line sudo-edit smooth-scrolling smex rainbow-delimiters python-mode peep-dired org-bullets magit lsp-ui lsp-treemacs lsp-ivy ivy-rich ivy-posframe general gcmh exwm evil-nerd-commenter evil-collection eshell-git-prompt emojify elfeed-goodies doom-themes dmenu dired-single dired-open desktop-environment counsel-projectile company-box auto-package-update all-the-icons-dired alert)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
